2d691b644d921911d212f7b0fc3a6de113afc063
Robert O. Pedersen <rop@con-linq.com>
2026-08-18 14:36:18 +0200

    Added mosquitto listener

---
+++ Services/SensorDataService.cs
@@ -0,0 +1,34 @@
+﻿using SmartHomeIoT.Api.Data;
+using SmartHomeIoT.Api.Models;
+
+namespace SmartHomeIoT.Api.Services;
+public class SensorDataService
+{
+    private readonly SmartHomeDbContext _context;
+
+    public SensorDataService(SmartHomeDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SaveSensorDataAsync(
+        int deviceId,
+        string sensorType,
+        decimal value,
+        string unit,
+        DateTime timestamp)
+    {
+        var sensorData = new SensorData
+        {
+            DeviceId = deviceId,
+            SensorType = sensorType,
+            Value = value,
+            Unit = unit,
+            Timestamp = timestamp
+        };
+
+        _context.SensorData.Add(sensorData);
+
+        await _context.SaveChangesAsync();
+    }
+}
